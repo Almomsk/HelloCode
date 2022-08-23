6d7b4b2d473bc063e57ee350951a08b7d55433b6
Alexander <Almomsk@gmail.com>
2022-08-23 14:38:45 +0300

    Add Example005_ConditionIfElse project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "alex")
+{
+Console.WriteLine("Ура, это же Alex!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
